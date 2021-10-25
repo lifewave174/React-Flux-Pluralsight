3f4f0850a1d637ef79be1415010278eed013afc5
dci <priya.shah174@gmail.com>
2021-10-25 17:22:04 +0200

    created authors store and populated authors in state in coursespage

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import courseStore from '../stores/courseStore';
+import authorStore from '../stores/authorStore';
 import CourseList from './CourseList';
 import { Link } from 'react-router-dom';
 import { loadCourses, deleteCourse } from '../actions/courseActions';
+import { loadAuthors } from '../actions/authoractions';
 
 function CoursesPage() {
   const [courses, setCourses] = useState(courseStore.getCourses());
+  const [authors, setAuthors] = useState(authorStore.getAuthors());
 
   useEffect(() => {
     courseStore.addChangeListener(onChange);
@@ -13,8 +16,15 @@ function CoursesPage() {
     return () => courseStore.removeChangeListener(onChange); //clean up on unmount
   }, []);
 
+  useEffect(() => {
+    authorStore.addChangeListener(onChange);
+    if (authorStore.getAuthors().length === 0) loadAuthors();
+    return () => authorStore.removeChangeListener(onChange);
+  }, []);
+
   function onChange() {
     setCourses(courseStore.getCourses());
+    setAuthors(authorStore.getAuthors()); //populating author and course data here from the store
   }
 
   return (
