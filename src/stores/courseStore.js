c5c55d5211adf74249ddbd452e35bcd700123df0
dci <priya.shah174@gmail.com>
2021-10-25 13:36:48 +0200

    done with registering the dispatcher with action types and creating functions to export parts of the store to the components

--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -1,7 +1,10 @@
 import { EventEmitter } from 'events';
 import Dispatcher from '../appDispatcher';
+import actionTypes from '../actions/actionTypes';
 
 const CHANGE_EVENT = 'change';
+let _courses = [];
+
 class CourseStore extends EventEmitter {
   addChangeListener(callback) {
     this.on(CHANGE_EVENT, callback);
@@ -14,12 +17,26 @@ class CourseStore extends EventEmitter {
   emitChange() {
     this.emit(CHANGE_EVENT);
   }
+
+  getCourses() {
+    return _coruses;
+  }
+
+  getCourseBySlug(slug) {
+    return _courses.find((course) => course.slug === slug);
+  }
 }
 
 const store = new CourseStore();
 
 Dispatcher.register((action) => {
   switch (action.actionType) {
+    case actionTypes.CREATE_COURSE:
+      _courses.push(action.course);
+      store.emitChange();
+      break;
+    default:
+    //nothing to do here
   }
 });
 
